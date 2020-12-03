e45607f2f94eb17ccaddd1a507957c866c796318
Caio Taulois <caiotaulois@gmail.com>
2020-12-02 23:43:52 -0300

    Transformação para metódos funcionais

--- src/main/java/br/com/radix/banco/BancoController.java
+++ src/main/java/br/com/radix/banco/BancoController.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/banco")
 public class BancoController {
@@ -30,12 +31,8 @@ public class BancoController {
     @Produces(MediaType.APPLICATION_JSON)
     public Conta obterConta(@PathParam("numero") Long numero) {
 
-        for (Conta conta : contas) {
-            if (conta.getNumero().equals(numero)) {
-                return conta;
-            }
-        }
-        throw new NotFoundException();
+        Optional<Conta> contaEncontrada = contas.stream().filter(conta -> conta.getNumero().equals(numero)).findFirst();
+        return contaEncontrada.orElseThrow(NotFoundException::new);
     }
 
     @POST
@@ -52,16 +49,8 @@ public class BancoController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response editarConta(@PathParam("numero") Long numero, Conta requisicaoEditarConta) {
 
-        Conta contaEditada = null;
-        for (Conta conta : contas) {
-            if (conta.getNumero().equals(numero)) {
-                contaEditada = conta;
-            }
-        }
-        if (contaEditada == null) {
-            throw new NotFoundException();
-        }
-        contaEditada.atualizarDados(requisicaoEditarConta.getCliente(), requisicaoEditarConta.getSaldo());
+        Optional<Conta> contaEditada = contas.stream().filter(conta -> conta.getNumero().equals(numero)).findFirst();
+        contaEditada.orElseThrow(NotFoundException::new).atualizarDados(requisicaoEditarConta.getCliente(), requisicaoEditarConta.getSaldo());
         return Response.ok().build();
     }
 
@@ -70,16 +59,8 @@ public class BancoController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response deletarConta(@PathParam("numero") Long numero) {
 
-        Conta contaExcluida = null;
-        for (Conta conta : contas) {
-            if (conta.getNumero().equals(numero)) {
-                contaExcluida = conta;
-            }
-        }
-        if (contaExcluida == null) {
-            throw new NotFoundException();
-        }
-        contas.remove(contaExcluida);
+        Optional<Conta> contaExcluida = contas.stream().filter(conta -> conta.getNumero().equals(numero)).findFirst();
+        contas.remove(contaExcluida.orElseThrow(NotFoundException::new));
         return Response.ok().build();
     }
 }
