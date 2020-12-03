5aeb7cfeaf6d1755f5b22d10a040b194aa26a698
Caio Taulois <caiotaulois@gmail.com>
2020-12-02 22:28:09 -0300

    Inclusão de todas as rotas de CRUD de /conta

--- src/main/java/br/com/radix/banco/BancoController.java
+++ src/main/java/br/com/radix/banco/BancoController.java
@@ -1,9 +1,14 @@
 package br.com.radix.banco;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,5 +25,38 @@ public class BancoController {
         contas.add(new Conta("cliente 2", 2L, 20.0));
         contas.add(new Conta("cliente 3", 3L, 30.0));
         return contas;
+    }
+
+    @GET
+    @Path("/conta/{numero}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Conta obterConta(@PathParam("numero") Long numero) {
+
+        Conta conta = new Conta("cliente teste", numero, 0.0);
+        return conta;
+    }
+
+    @POST
+    @Path("/conta")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response criarConta() {
+
+        return null;
+    }
+
+    @PUT
+    @Path("/conta/{numero}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response atualizarConta(@PathParam("numero") Long numero) {
+
+        return null;
+    }
+
+    @DELETE
+    @Path("/conta/{numero}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deletarConta(@PathParam("numero") Long numero) {
+
+        return null;
     }
 }
